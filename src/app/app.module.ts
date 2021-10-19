f0983584bb563220d1e29c10259f638c7ba781ea
Andrews Vinícius <andykenobi@gmail.com>
2021-10-19 14:21:47 -0300

    Make the base Project conguration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatCardModule} from '@angular/material/card';
+
+import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './dashboard/users/user-list/user-list.component';
+import { UserEditComponent } from './dashboard/users/user-edit/user-edit.component';
+import { UserShowComponent } from './dashboard/users/user-show/user-show.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserListComponent,
+    UserEditComponent,
+    UserShowComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
